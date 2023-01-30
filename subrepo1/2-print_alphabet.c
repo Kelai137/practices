35754e12d5516f0db7b22e650918565bc68068e1
Kelai137 <moseskelaisakala137@gmail.com>
2023-01-30 06:26:49 -0800

    haha

---
+++ subrepo1/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check code
+ * print lower case letter
+ * a to z
+ *
+ * return 0.
+ */
+
+void print_lowercase_alphabet(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+}
+
+int main(void)
+{
+	print_lowercase_alphabet();
+	return (0);
+}
